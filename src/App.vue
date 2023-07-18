0f775e6cd594333587ac1792bc0fc7a9eab1785f
Michal Staniecko <michal.staniecko@gmail.com>
2023-07-18 22:27:55 +0200

    add bulma

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,11 @@
 </script>
 
 <template>
-  <RouterLink to="/">Notes</RouterLink>
-   | 
-  <RouterLink to="/stats">Stats</RouterLink>
+  <RouterLink to="/" class="button">Notes</RouterLink>
+  <RouterLink to="/stats" class="button">Stats</RouterLink>
   <RouterView/>
 </template>
 
-<style scoped>
-
+<style>
+@import 'bulma/css/bulma.min.css';
 </style>
