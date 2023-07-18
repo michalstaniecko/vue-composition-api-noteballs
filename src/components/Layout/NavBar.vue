38f668063452e860771b1240f086999db4766aed
Michal Staniecko <michal.staniecko@gmail.com>
2023-07-18 22:41:14 +0200

    add mobile menu feature

--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -1,40 +1,50 @@
 <script setup>
+import { ref } from 'vue';
 
+const showMobileNav = ref( false );
 </script>
 
 <template>
   <nav class="navbar is-success" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <div class="navbar-item is-size-4 is-family-monospace" href="https://bulma.io">
-        Noteballs
+    <div class="container is-max-desktop">
+      <div class="navbar-brand">
+        <div class="navbar-item is-size-4 is-family-monospace" href="https://bulma.io">
+          Noteballs
+        </div>
+
+        <a role="button" @click.prevent="showMobileNav = !showMobileNav" class="navbar-burger" :class="{'is-active': showMobileNav}" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
 
-      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-
-    <div id="navbarBasicExample" class="navbar-menu">
-      <div class="navbar-end">
-        <RouterLink
-            class="navbar-item"
-            to="/"
-            active-class="is-active"
-        >Notes
-        </RouterLink>
-        <RouterLink
-            class="navbar-item"
-            to="/stats"
-            active-class="is-active"
-        >Stats
-        </RouterLink>
+      <div id="navbarBasicExample" class="navbar-menu" :class="{'is-active': showMobileNav}">
+        <div class="navbar-end">
+          <RouterLink
+              class="navbar-item"
+              to="/"
+              active-class="is-active"
+          >Notes
+          </RouterLink>
+          <RouterLink
+              class="navbar-item"
+              to="/stats"
+              active-class="is-active"
+          >Stats
+          </RouterLink>
+        </div>
       </div>
     </div>
   </nav>
 </template>
 
 <style scoped>
-
+  @media (max-width: 1023px) {
+    .navbar-menu {
+      position: absolute;
+      left: 0;
+      width: 100%;
+    }
+  }
 </style>
