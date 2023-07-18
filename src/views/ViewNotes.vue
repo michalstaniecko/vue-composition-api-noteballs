3548198e91a12c1b6c426c25aebf0a80feb101c0
Michal Staniecko <michal.staniecko@gmail.com>
2023-07-18 22:25:13 +0200

    add router

---
+++ src/views/ViewNotes.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="notes">
+    <h1>Notes</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/ViewStats.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="stats">
+    <h1>Stats</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
