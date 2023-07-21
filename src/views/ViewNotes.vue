1739523a026a0ecbcc1cfa7ce177a1ae0efc4dc5
Michal Staniecko <michal.staniecko@gmail.com>
2023-07-21 21:38:06 +0200

    handle notes using refs

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,5 +1,32 @@
 <script setup>
+import { ref } from "vue";
 
+const newNote = ref( '' );
+
+const newNoteRef = ref(null);
+
+const notes = ref( [
+  {
+    id: 'id1',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda atque deserunt earum eum inventore officia quaerat soluta ullam vel. Dolore facere fugit itaque laboriosam maxime quis ratione soluta tenetur. Beatae'
+  },
+  {
+    id: 'id2',
+    content: 'shorter notes'
+  }
+] );
+
+const addNote = () => {
+  const currentDate = new Date().getTime()
+  const id = currentDate.toString();
+  const note = {
+    id,
+    content: newNote.value
+  }
+  notes.value.unshift(note);
+  newNote.value = '';
+  newNoteRef.value.focus();
+}
 </script>
 
 <template>
@@ -7,22 +34,27 @@
     <div class="card has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
-          <textarea class="textarea" placeholder="Add a new note"></textarea>
+          <textarea
+              ref="newNoteRef"
+              v-model="newNote"
+              class="textarea"
+              placeholder="Add a new note"></textarea>
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success">Add New Note</button>
+          <button
+              :disabled="!newNote"
+              @click="addNote" class="button is-link has-background-success"
+          >Add New Note
+          </button>
         </div>
       </div>
     </div>
-    <div class="card mb-4" v-for="i in 3">
+    <div class="card mb-4" :key="note.id" v-for="note in notes">
       <div class="card-content">
-        <div class="content">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda atque deserunt earum eum inventore officia
-          quaerat soluta ullam vel. Dolore facere fugit itaque laboriosam maxime quis ratione soluta tenetur. Beatae.
-        </div>
+        <div class="content">{{ note.content }}</div>
       </div>
       <footer class="card-footer">
         <a href="#" class="card-footer-item">Edit</a>
