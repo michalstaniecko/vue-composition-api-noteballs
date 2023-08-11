a3b7267a53729f63bc0352bd9dfaf3cff41b9212
Michal Staniecko <michal.staniecko@gmail.com>
2023-08-11 14:00:10 +0200

    handle navigation guard

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import ViewNotes from "@/views/ViewNotes.vue";
 import ViewStats from "@/views/ViewStats.vue";
 import ViewEditNote from "@/views/ViewEditNote.vue";
 import ViewAuth from "@/views/ViewAuth.vue";
+import { useStoreAuth } from "@/stores/storeAuth";
 
 const routes = [
     {
@@ -30,6 +31,16 @@ const routes = [
 const router = createRouter( {
     history: createWebHashHistory(),
     routes
+} );
+
+router.beforeEach( async ( to, from ) => {
+    const storeAuth = useStoreAuth();
+    if (!storeAuth.isLogged && to.name !== 'auth') {
+        return { name: 'auth' }
+    }
+    if (storeAuth.isLogged && to.name === 'auth') {
+        return false;
+    }
 } )
 
 export default router;
